18faa1ca1766b46738773fbbff47fb6158f686ea
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-09-27 00:45:55 +0100

    Hover functionality to the Navigation Menu

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('.menu').on('mouseenter', ()=>{
+      $('.nav-menu').show();
+    }).on('mouseleave', () =>{
+      $('.nav-menu').hide();
+    });
+    
+    $('.btn').on('mouseenter', (event) =>{
+      $(event.currentTarget).addClass('.btn-hover')
+    }).on('mouseleave',(event) =>{
+      $(event.currentTarget).removeClass('btn-hover');
+    })
+    }); 
+    

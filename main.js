ba51357dd71a0c6efbe1a0277f8acff3506a66fc
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-09-27 01:05:23 +0100

    Limit a users post to 140  Menu

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 $(document).ready(() => {
-    $('.menu').on('mouseenter', ()=>{
-      $('.nav-menu').show();
-    }).on('mouseleave', () =>{
-      $('.nav-menu').hide();
-    });
-    
-    $('.btn').on('mouseenter', (event) =>{
-      $(event.currentTarget).addClass('.btn-hover')
-    }).on('mouseleave',(event) =>{
-      $(event.currentTarget).removeClass('btn-hover');
-    })
-    }); 
-    
+
+    //Event Listener for mouse enter
+  $('.menu').on('mouseenter', ()=>{
+    $('.nav-menu').show();
+  }).on('mouseleave', () =>{
+    $('.nav-menu').hide();
+  });
+  
+  $('.btn').on('mouseenter', (event) =>{
+    $(event.currentTarget).addClass('.btn-hover')
+  }).on('mouseleave',(event) =>{
+    $(event.currentTarget).removeClass('btn-hover');
+  })
+  
+  //Below add event for limiting users post characters
+  $('.postText').on('keyup', ()=>{
+    $('.postText').focus();
+  
+    let post = $(event.currentTarget).val();
+    let remaining = 140 - post.length;
+    if(remaining <= 0){
+      $('.wordcount').addClass('red');
+    }else{
+       $('.wordcount').removeClass('red');
+    }
+    $('.characters').html(remaining); //this will change test of html
+  })
+  
+  }); 
+  
